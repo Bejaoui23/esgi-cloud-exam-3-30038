fde07f1e5f3c81030cd4ddf67c4b68c44d1d9761
Mustapha <bejaouimostfa01@gmail.com>
2024-04-02 13:07:59 +0200

    tt

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   s3: {
     accessKeyId: 'HLXI7LPKFW9FD7PPA5WW', // TODO
     secretAccessKey: 'onrAmzGe5V41dtbL1lfZFdItEeqACHZgTeXXA2fF', // TODO
-    endpoint: 'https://s3.eu-west-2.wasabisys.com/sgi-cloud-exam-bucket-30038/Formation_ESGI.jpg', // TODO
+    endpoint: 'https://s3.eu-west-2.wasabisys.com/sgi-cloud-exam-bucket-30038/', // TODO
     bucket: 'sgi-cloud-exam-bucket-30038', // TODO
   },
   suffix: {

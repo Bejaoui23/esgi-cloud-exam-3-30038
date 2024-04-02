aa2d5898e257ba21a5f316e7f274919a1ee93503
Mustapha <bejaouimostfa01@gmail.com>
2024-04-02 12:50:45 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'HLXI7LPKFW9FD7PPA5WW', // TODO
+    secretAccessKey: 'onrAmzGe5V41dtbL1lfZFdItEeqACHZgTeXXA2fF', // TODO
+    endpoint: 'https://s3.eu-west-2.wasabisys.com/sgi-cloud-exam-bucket-30038/Formation_ESGI.jpg', // TODO
+    bucket: 'sgi-cloud-exam-bucket-30038', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
